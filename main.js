f8aeacf0fb39881685ff51b63c4fa7fcee1e1d41
Jarol Riera <80286893+HaroldTheCoder@users.noreply.github.com>
2021-09-19 18:10:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// Type writer header
+function typingEffect(element, speed) {
+    let text= element.innerHTML;
+    element.innerHTML="";
+    var i= 0;
+    var timer = setInterval(function() {
+        if(i < text.length) {
+            element.append(text.charAt(i))
+            i++;
+        }else {
+            clearInterval(timer);
+        }
+    }, speed)
+
+}
+const title = document.querySelector("h1");
+typingEffect(title, 120);
+
+//Navbar function
+
+const menuButton = document.querySelector(".button-container");
+const closeButton = document.querySelector(".menu-close-button");
+const navBar = document.querySelector("#main-navbar");
+const navLink = document.querySelectorAll('.nav-link');
+
+menuButton.addEventListener('click', () => {
+    navBar.classList.toggle('active');
+    document.body.classList.toggle('inactive');
+});
+
+
+closeButton.addEventListener('click', () => {
+    navBar.classList.remove('active');
+    document.body.classList.toggle("inactive");
+})
+
+navLink.forEach((n) => n.addEventListener('click', () => {
+    navBar.classList.remove('active');
+    document.body.classList.toggle("inactive");
+}));
